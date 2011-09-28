30be9732115cf1f4cac0e920d045f3e9c63661d5
jewkesy <jewkesy@1969f786-7071-46a5-897b-61d37f95235e>
2011-09-28 13:26:38 +0000

    All done;

---
+++ BowlingGameKata/BowlingGameKata/Game.cs
@@ -0,0 +1,65 @@
+﻿namespace BowlingGameKata
+{
+    public class Game
+    {
+        private readonly int[] _rolls = new int[21]; //max of 21 rolls in a game
+        private int _currentRoll;
+
+        public void Roll(int pins)
+        {
+            _rolls[_currentRoll++] = pins;
+        }
+
+        public int Score()
+        {
+            int score = 0;
+            int roll = 0;
+
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if (IsStrike(roll))
+                {
+                    score += 10 + StrikeBonus(roll);
+                    roll++;
+                }
+                else if (IsSpare(roll))
+                {
+                    score += 10 + SpareBonus(roll);
+                    roll += 2;
+                }
+                else
+                {
+                    score += SumOfBallsInFrame(roll);
+                    roll += 2;
+                }
+            }
+
+            return score;
+        }
+
+        private bool IsStrike(int roll)
+        {
+            return _rolls[roll] == 10;
+        }
+
+        private bool IsSpare(int roll)
+        {
+            return _rolls[roll] + _rolls[roll + 1] == 10;
+        }
+
+        private int SumOfBallsInFrame(int roll)
+        {
+            return _rolls[roll] + _rolls[roll + 1];
+        }
+
+        private int SpareBonus(int roll)
+        {
+            return _rolls[roll + 2];
+        }
+
+        private int StrikeBonus(int roll)
+        {
+            return _rolls[roll + 1] + _rolls[roll + 2];
+        }
+    }
+}
---
+++ BowlingGameKata/BowlingGameKataTest/GameTest.cs
@@ -0,0 +1,85 @@
+﻿using NUnit.Framework;
+using BowlingGameKata;
+
+namespace BowlingGameKataTest
+{
+    [TestFixture]
+    public class GameTest
+    {
+        private Game _g;
+
+        [Test]
+        public void TestGutterGame()
+        {
+            RollMany(20, 0);
+
+            Assert.That(_g.Score(), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void TestAllOnes()
+        {
+            RollMany(20, 1);
+
+            Assert.That(_g.Score(), Is.EqualTo(20));
+        }
+
+        [Test]
+        public void TestOneSpare()
+        {
+            RollSpare();
+            _g.Roll(3);
+            RollMany(17, 0);
+            
+            Assert.That(_g.Score(), Is.EqualTo(16));
+        }
+
+        [Test]
+        public void TestOneStrike()
+        {
+            RollStrike();
+            _g.Roll(3);
+            _g.Roll(4);
+            RollMany(16, 0);
+            Assert.That(_g.Score(), Is.EqualTo(24));
+        }
+
+        [Test]
+        public void TestPerfectGame()
+        {
+            RollMany(12, 10);
+            Assert.That(_g.Score(), Is.EqualTo(300));
+        }
+
+        private void RollStrike()
+        {
+            _g.Roll(10);
+        }
+
+        private void RollSpare()
+        {
+            _g.Roll(5);
+            _g.Roll(5);
+        }
+
+        private void RollMany(int rolls, int pins)
+        {
+            for (int i = 0; i < rolls; i++)
+            {
+                _g.Roll(pins);
+            }
+        }
+
+        [SetUp]
+        public void TestFixtureSetUp()
+        {
+            _g = new Game();
+        }
+
+        [TearDown]
+        public void TestFixtureTearDown()
+        {
+            _g = null;
+        }
+    }
+}
